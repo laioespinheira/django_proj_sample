d9d98dc3fe8c4f198ee24f0014d7b641289f6d11
Laio Espinheira <laioespinheira@gmail.com>
2024-09-26 22:08:52 +0800

    Hello World msg

---
+++ src/saas_proj/views.py
@@ -0,0 +1,40 @@
+import pathlib
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "total_visit_count": qs.count(),
+    }
+    path = request.path
+    print(path)
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, my_context)
+
+def my_old_home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_ = """
+    <!DOCTYPE html>
+    <html>
+
+    <body>
+        <h1>Yo {page_title} anything?</h1>
+    </body>
+    </html>
+    """.format(**my_context)
+    #html_file_path = this_dir / "home.html"
+    #html_ = html_file_path.read_text()
+    return HttpResponse(html_)
